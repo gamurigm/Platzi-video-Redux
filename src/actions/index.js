4343a51628c5219d11622642fb8db08c66b6116f
Gabriel Murillo Medina <gamurigm@gmail.com>
2020-06-26 17:53:53 -0500

    remove icon

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+//Actions
+
+
+export const setFavorite = (payload) => ({
+    type: 'SET_FAVORITE',
+    payload,
+});
+
+export const deleteFavorite = (payload) => ({
+    type: 'DELETE_FAVORITE ',
+    payload,
+})
